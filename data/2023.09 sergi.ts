b2dd2025f8f0651e9e05e4ddfdebd1d620014457
Sergi Juanati <wint3rmute@gmail.com>
2023-10-04 09:40:56 +0200

    Added json data

---
+++ data/2023.09 sergi.ts
@@ -0,0 +1,17 @@
+const Sergi = {
+    focus: ['blockchain', 'business intelligence', 'data science', 'artificial intelligence'],
+    tech: {
+        programming_languages: ['JavaScript', 'TypeScript', 'AssemblyScript', 'Python', 'Solidity', 'HTML', 'CSS'],
+        query_languages: ['SQL', 'GraphQL'],
+        frameworks: ['Node.js', 'Express', 'React'],
+        web3_tools: ['Subgraphs', 'Dune'],
+        databases: ['Oracle', 'PostgreSQL'],
+        colab_tools: ['GitHub', 'Discord', 'Slack', 'Confluence', 'Linear', 'Notion', 'Trello', 'Jira'],
+        misc: ['SAP', 'MicroStrategy']
+    },
+    crypto: {
+        wallets: ['Rabby', 'Metamask', 'Argent/X', 'Braavos'],
+        airdropping: ['zkSync', 'Starknet', 'LayerZero'],
+        following: ['r/st/stf/ETH', 'GRT', 'MATIC', 'ADA', 'AVAX', 'SAND', 'MANA', 'DOGE', 'CRO', 'BTC']
+    }
+}
